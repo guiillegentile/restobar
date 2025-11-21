2739cd777cbad86a3f3f927007c1478fecdd69cf
guillegentile <guillugen@gmail.com>
2025-11-21 17:17:04 -0300

    hola

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainPage } from './pages/main-page/main-page';
-import { LoginOwner } from './pages/login-owner/login-owner';
-import { LoginClient } from './pages/login-client/login-client';
-import { RegisterClient } from './pages/register-client/register-client';
+import { LoginOwnerComponent } from './pages/login-owner/login-owner';
+import { LoginPage } from './pages/login-client/login-client';
+import { RegisterPage } from './pages/register-client/register-client';
 import { MenuRestaurantPage } from './pages/menu-restaurant/menu-restaurant';
 
 export const routes: Routes = [
@@ -12,7 +12,7 @@ export const routes: Routes = [
   },
   {
     path: 'login-owner',
-    component: LoginOwner
+    component: LoginOwnerComponent
   },
   {
     path: 'menu/:id',
@@ -20,11 +20,11 @@ export const routes: Routes = [
   },
   {
     path: 'login-client',
-    component: LoginClient
+    component: LoginPage    // 👈 usa LoginPage, no LoginClient
   },
   {
     path: 'register-client',
-    component: RegisterClient
+    component: RegisterPage
   },
   {
     path: '**',
