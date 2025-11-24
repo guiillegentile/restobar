fc7b46d26e913c375ee5e7ec805824c1df819263
Julia Venesia <jvenesiamenna@gmail.com>
2025-11-24 10:25:32 -0300

    hoy

--- src/app/pages/categories/categories.ts
+++ src/app/pages/categories/categories.ts
@@ -1,11 +1,103 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuService } from '../../services/menu-service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CategoryService } from '../../services/categories-service';
+import { Spinner } from '../../components/spinner/spinner';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './categories.html',
   styleUrl: './categories.css',
 })
-export class Categories {
+export class Categories implements OnInit {
+
+  router = inject(Router);
+  menuService = inject(MenuService);
+  _categoryService = inject(CategoryService)
+cdr = inject(ChangeDetectorRef);
+
+  categories: any[] = [];
+  isEditing = false;
+  categoryData = { id: '', name: '' };
+  isLoading = false; 
+
+  ngOnInit() {
+    this.loadCategories();
+  }
+
+  async loadCategories() {
+    this.isLoading = true; 
+
+    try {
+      this.categories = await this._categoryService.getCategories();
+
+    } catch (error) {
+      console.error('Hubo un error cargando las categorías:', error);
+    } finally {this.cdr.detectChanges();
+      this.isLoading = false;
+      console.log(this.isLoading)
+    }
+  }
+
+  async saveCategory(form: any) {
+    if (form.invalid) return;
+
+    this.isLoading = true; 
+
+    try {
+      if (this.isEditing) {
+        try {
+          await this._categoryService.editCategory(this.categoryData.id, this.categoryData.name);
+          
+        } catch (error) {
+          
+        }finally{
+          this.cdr.detectChanges();
+        }
+      } else {
+        try {
+          await this._categoryService.createCategory(this.categoryData.name);
+          
+        } catch (error) {
+          
+        }finally{
+          this.cdr.detectChanges();
+        }
+      }
+      this.cancelEdit();
+      await this.loadCategories();
+
+    } catch (error) {
+      alert('Error al guardar la categoría. Verifique su conexión o sesión.');
+    } finally {
+      this.isLoading = false; 
+    }
+  }
+
+  async deleteCategory(id: string) {
+    if (!confirm('¿Seguro que deseas eliminar esta categoría?')) return;
+
+    try {
+      this.isLoading = true;
+      await this._categoryService.deleteCategory(id);
+      await this.loadCategories();
+    } catch (error) {
+      alert('No se pudo eliminar la categoría.');
+    } finally {
+      this.isLoading = false;
+      this.cdr.detectChanges();
+    }
+  }
+  selectCategoryToEdit(cat: any) {
+    this.isEditing = true;
+    this.categoryData = { ...cat };
+  }
+  cancelEdit() {
+    this.isEditing = false;
+    this.categoryData = { id: '', name: '' };
+  }
 
 }
---
+++ src/app/services/categories-service.ts
@@ -0,0 +1,98 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { firstValueFrom } from "rxjs";
+
+export interface Category {
+  id: string;
+  name: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class CategoryService {
+  private _apiUrl = 'https://w370351.ferozo.com/api';
+  private _httpClient = inject(HttpClient);
+
+  
+  private getHeaders(): HttpHeaders {
+    const rawToken = localStorage.getItem('token');
+    const token = rawToken ? rawToken.replace(/"/g, '') : '';
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
+  
+  private getUserIdFromToken(): string | null {
+    const token = localStorage.getItem('token');
+    if (!token) return null;
+    try {
+      const payloadPart = token.split('.')[1];
+      const payload = JSON.parse(atob(payloadPart));
+      return payload.sub;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  
+  async getCategories(): Promise<Category[]> {
+    try {
+      const userId = this.getUserIdFromToken();
+      if (!userId) throw new Error('Usuario no identificado');
+
+      return await firstValueFrom(
+        this._httpClient.get<Category[]>(`${this._apiUrl}/users/${userId}/categories`)
+      );
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+
+  async createCategory(name: string) {
+    try {
+      return await firstValueFrom(
+        this._httpClient.post(
+          `${this._apiUrl}/categories`, 
+          { name }, 
+          { headers: this.getHeaders() }
+        )
+      );
+    } catch (error) {
+      console.error(error);
+      throw error; 
+    }
+  }
+
+  
+  async editCategory(id: string, name: string) {
+    
+    try {
+      return await firstValueFrom(
+        this._httpClient.put(
+          `${this._apiUrl}/categories/${id}`, 
+          { name }, 
+          { headers: this.getHeaders() }
+        )
+      );
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+
+  async deleteCategory(id: string) {
+    try {
+      return await firstValueFrom(
+        this._httpClient.delete(
+          `${this._apiUrl}/categories/${id}`, 
+          { headers: this.getHeaders() }
+        )
+      );
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+}
